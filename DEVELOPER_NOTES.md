e33e5dd0d839db28c4765f9d4b3960ca98a3bdae
Apurbo Roy <apurboroy7077@gmail.com>
2024-02-11 13:02:28 +0600

    Initialized Tailwind and Fixed Some Stuffs, Date 11/2/2024, Sunday.

---
+++ DEVELOPER_NOTES.md
@@ -0,0 +1,2 @@
+1. Date 11/2/2024, Sunday.
+   1. Installed Tailwind to my Project and Some Stylings Gone Wrong. Also had to restart npm run dev to make tailwind work.
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Portfolio_index from "./Apps/Portfolio/Portfolio_index";
-
+import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Portfolio_index />
